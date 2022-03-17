d30271ad4ca73baa828a0c20ffbe3f2d0c1c47e2
vouu <contact@manhtuong.net>
2022-03-17 11:19:09 +0700

    ARM Support

--- installing_deps.sh
+++ installing_deps.sh
@@ -82,7 +82,7 @@ sudo make install
 popd
 
 # ARDB #
-test ! -d ardb/ && git clone https://github.com/ail-project/ardb.git
+test ! -d ardb/ && git clone https://github.com/My-WAF/ardb.git
 pushd ardb/
 make
 popd
